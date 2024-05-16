ae12fe4bafb680a53f9fed7d78c4b253ac832e79
sahil <sahil.@sahils-MacBook-Air.local>
2024-05-16 17:49:36 -0400

    Added finnhub code

--- StockMarketFinalProject/app.js
+++ StockMarketFinalProject/app.js
@@ -3,12 +3,17 @@ const path = require("path");
 const express = require("express"); 
 const app = express(); 
 const bodyParser = require("body-parser");
+const finnhub = require('finnhub');
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "Cp2ges1r01qh00mjb9ugcp2ges1r01qh00mjb9v0";
 require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') }) 
 const userName = process.env.MONGO_DB_USERNAME;
 const password = process.env.MONGO_DB_PASSWORD;
 const {MongoClient, ServerApiVersion} = require('mongodb');
 const databaseAndCollection = {db: process.env.MONGO_DB_NAME, collection:process.env.MONGO_COLLECTION};
-const uri = 'mongodb+srv://liamgedeon1:CQ59EYtA@cluster0.l3sf3ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri = 'mongodb+srv://liamgedeon1:CQ59EYtA@cluster0.l3sf3ic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const finnhubClient = new finnhub.DefaultApi();
+
 
 process.stdin.setEncoding("utf8");
 app.set("views", path.resolve(__dirname, "templates"));
@@ -26,6 +31,9 @@ app.listen(portNumber);
 console.log(`Web server started and running at http://localhost:${portNumber}`);
 const prompt = "Type stop to shutdown the server:";
 console.log(prompt);
+finnhubClient.symbolSearch('AAPL', (error, data, response) => {
+    console.log(data)
+});
 
 process.stdin.on('readable', () => {
 
